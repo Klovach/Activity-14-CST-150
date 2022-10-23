ed9a1f113e2466253e0399cf52c51bbf8be6fe3b
elisa <elisa@DESKTOP-NOHK2QH>
2022-10-23 11:42:11 -0700

    Add project files.

---
+++ Activity 14 CST_150/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Activity_14_CST_150
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.fontList = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.lblDecor = new System.Windows.Forms.Label();
+            this.lblTextColor = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtMessage.Location = new System.Drawing.Point(26, 68);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(390, 413);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.Text = "";
+            // 
+            // fontList
+            // 
+            this.fontList.FormattingEnabled = true;
+            this.fontList.ItemHeight = 16;
+            this.fontList.Items.AddRange(new object[] {
+            "Times New Roman",
+            "Arial",
+            "Palatino Linotype",
+            "Montserrat",
+            "Mongolian Baiti",
+            "MV Boli",
+            "Microsoft Sans Serif"});
+            this.fontList.Location = new System.Drawing.Point(421, 225);
+            this.fontList.Name = "fontList";
+            this.fontList.Size = new System.Drawing.Size(329, 116);
+            this.fontList.TabIndex = 1;
+            this.fontList.SelectedIndexChanged += new System.EventHandler(this.fontList_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Maroon;
+            this.radioButton1.Location = new System.Drawing.Point(603, 377);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.ChangeTextColour);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Teal;
+            this.radioButton2.Location = new System.Drawing.Point(602, 404);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Blue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.ChangeTextColour);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(421, 380);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Bold";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(421, 407);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 20);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Italic";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.Color.Green;
+            this.radioButton3.Location = new System.Drawing.Point(603, 433);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 20);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Green";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.ChangeTextColour);
+            // 
+            // lblDecor
+            // 
+            this.lblDecor.AutoSize = true;
+            this.lblDecor.Location = new System.Drawing.Point(420, 358);
+            this.lblDecor.Name = "lblDecor";
+            this.lblDecor.Size = new System.Drawing.Size(102, 16);
+            this.lblDecor.TabIndex = 9;
+            this.lblDecor.Text = "Decorative Text";
+            // 
+            // lblTextColor
+            // 
+            this.lblTextColor.AutoSize = true;
+            this.lblTextColor.Location = new System.Drawing.Point(599, 358);
+            this.lblTextColor.Name = "lblTextColor";
+            this.lblTextColor.Size = new System.Drawing.Size(68, 16);
+            this.lblTextColor.TabIndex = 10;
+            this.lblTextColor.Text = "Text Color";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(27, 487);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(146, 37);
+            this.btnNew.TabIndex = 11;
+            this.btnNew.Text = "New Document";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(179, 487);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 37);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(260, 487);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 37);
+            this.btnRead.TabIndex = 13;
+            this.btnRead.Text = "Read ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtTitle.Location = new System.Drawing.Point(26, 36);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(389, 26);
+            this.txtTitle.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Your Note:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Font Selection";
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.AllowUserToResizeColumns = false;
+            this.DataGridView.AllowUserToResizeRows = false;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(422, 68);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.RowHeadersVisible = false;
+            this.DataGridView.RowHeadersWidth = 51;
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(318, 112);
+            this.DataGridView.TabIndex = 17;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(341, 487);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 37);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(421, 434);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 20);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Underlined";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(422, 460);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(77, 20);
+            this.checkBox4.TabIndex = 19;
+            this.checkBox4.Text = "Regular";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(602, 459);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(62, 20);
+            this.radioButton4.TabIndex = 20;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.ChangeTextColour);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(713, 481);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 37);
+            this.btnExit.TabIndex = 21;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Note Selection";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lblTextColor);
+            this.Controls.Add(this.lblDecor);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.fontList);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Activity 14 CST-150 - Note Taking Application";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.ListBox fontList;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label lblDecor;
+        private System.Windows.Forms.Label lblTextColor;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Activity 14 CST_150/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Activity_14_CST_150
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /* Here, upon loading the foam, we add a new DataTable.
+         We add two columns to that table, a title column and a messages column.
+        For visual purposes, we hide the messages column and make the width of the
+        title column equal to the data grid's width.*/
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            DataGridView.DataSource = table;
+
+            DataGridView.Columns["Messages"].Visible = false;
+            DataGridView.Columns["Title"].Width = DataGridView.Width;
+        }
+
+        // Here we let the user create a new document by clearing the screen of any text.
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            MessageBox.Show("You made a new document.");
+        }
+
+        /* Here we add our title text and message text to our rows in our datagrid table.
+         Then we clear the application of any text.*/
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        /* Here we let the user read the file. This is accomplished by getting
+         data from the table rows and replacing the blank text on the screen with the
+        text in our saved notes.*/
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int index = DataGridView.CurrentCell.RowIndex;
+
+            if (index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+
+            }
+        }
+
+        /* Here we delete a cell from the datagrid.*/
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = DataGridView.CurrentCell.RowIndex;
+            table.Rows[index].Delete();
+        }
+
+        // CHANGE FONT STYLE 
+        /* This is where we employ check boxes to let the user change the font style.
+         In each checkbox_CheckedChanged event, we invoke the UpdateFontStyle() method.*/
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        /* This is our method for changing the font. By using |=, we enable the user
+         to apply multiple font styles to the text. This is important because check boxes
+        are intended to let users select more than one option.*/
+        private void UpdateFontStyle()
+        {
+            System.Drawing.FontStyle style = System.Drawing.FontStyle.Regular;
+            if (checkBox1.Checked) style |= System.Drawing.FontStyle.Bold;
+            if (checkBox2.Checked) style |= System.Drawing.FontStyle.Italic;
+            if (checkBox3.Checked) style |= System.Drawing.FontStyle.Underline;
+            txtMessage.Font = new Font(txtMessage.Font, style);
+            txtTitle.Font = new Font(txtTitle.Font, style);
+        }
+
+
+        /* We employ a simple switch case to let the user choose a font from a list box.*/
+        private void fontList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int TextFont;
+            TextFont = fontList.SelectedIndex;
+            switch (TextFont)
+            {
+                case 0:
+                    Font timesFont = new Font("Times New Roman", 10.0f);
+                    txtMessage.Font = timesFont;
+                    txtTitle.Font = timesFont; 
+                    break;
+
+                case 1:
+                    Font arialFont = new Font("Arial", 10.0f);
+                    txtMessage.Font = arialFont;
+                    txtTitle.Font = arialFont;
+                    break;
+
+                case 2:
+                    Font palatinoFont = new Font("Palatino Linotype", 10.0f);
+                    txtMessage.Font = palatinoFont;
+                    txtTitle.Font = palatinoFont;
+                    break;
+
+                case 3:
+                    Font montserratFont = new Font("Montserrat", 10.0f);
+                    txtMessage.Font = montserratFont;
+                    txtTitle.Font = montserratFont;
+                    break;
+
+                case 4:
+                    Font mongolianFont = new Font("Mongolian Baiti", 10.0f);
+                    txtMessage.Font = mongolianFont;
+                    txtTitle.Font = mongolianFont;
+                    break;
+
+                case 5:
+                    Font boliFont = new Font("MV Boli", 10.0f);
+                    txtMessage.Font = boliFont;
+                    txtTitle.Font = boliFont;
+                    break;
+
+                case 6:
+                    Font defaultFont = new Font("Microsoft Sans Serif", 10.0f);
+                    txtMessage.Font = defaultFont;
+                    txtTitle.Font = defaultFont; 
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+
+        /* Here we can change the TextColor. Instead of having an event for every radio button in the
+         project, we change what happens in the radio buttons under properties and let them know to
+        employ this method when any of them are clicked.*/
+            private void ChangeTextColour(object sender, EventArgs e)
+            {
+                RadioButton tempButton = sender as RadioButton;
+
+                txtMessage.ForeColor = tempButton.ForeColor;
+                txtTitle.ForeColor = tempButton.ForeColor;
+            }
+
+        // Exit the application. 
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+    }
